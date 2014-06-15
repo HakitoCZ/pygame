26baf1008e2be974c8bec2fdd42518e227c1b641
HakitoCZ <hakito.cz@gmail.com>
2014-06-15 19:02:59 +0100

    refurnished

--- 0001.py
+++ 0001.py
@@ -8,13 +8,29 @@ Indent set to 2 spaces. I may be wrong but I feel it clearer that way.
 
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((640, 400))
 
-running = True
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-    if event.type == pygame.KEYDOWN and event.key == K_ESCAPE:
-      running = False
+class Game(object):
+  def main(self, screen):
+    while 1:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          return
+        if event.type == pygame.KEYDOWN and \
+            event.key == pygame.K_ESCAPE:
+          return
+
+if __name == '__main__':
+  pygame.init()
+  screen = pygame.display.set_mode((640, 400))
+  Game().main(screen)
+
+# pygame.init()
+# screen = pygame.display.set_mode((640, 400))
+# 
+# running = True
+# while running:
+#   for event in pygame.event.get():
+#     if event.type == pygame.QUIT:
+#       running = False
+#     if event.type == pygame.KEYDOWN and event.key == K_ESCAPE:
+#       running = False
