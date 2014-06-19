f9830e93afe30e4bc5aea1b5e7a76655a6465391
HakitoCZ <hakito.cz@gmail.com>
2014-06-19 22:25:02 +0100

    improoved collisions - issue

--- 0001.py
+++ 0001.py
@@ -19,18 +19,31 @@ class Player(pygame.sprite.Sprite):
     last = self.rect.copy()
 
     key = pygame.key.get_pressed()
-    if key[pygame.K_LEFT]:
-      self.rect.x -= 150 * dt
-    if key[pygame.K_RIGHT]:
-      self.rect.x += 150 * dt
-    if key[pygame.K_UP]:
-      self.rect.y -= 150 * dt
-    if key[pygame.K_DOWN]:
-      self.rect.y += 150 * dt
-        # movement speed 150px/s
+    if key[pygame.K_LEFT] or key[pygame.K_h]:
+      self.rect.x -= 250 * dt
+    if key[pygame.K_RIGHT] or key[pygame.K_l]:
+      self.rect.x += 250 * dt
+    if key[pygame.K_UP] or key[pygame.K_k]:
+      self.rect.y -= 250 * dt
+    if key[pygame.K_DOWN] or key[pygame.K_j]:
+      self.rect.y += 250 * dt
+        # movement speed 250px/s
 
+    new = self.rect
     for cell in pygame.sprite.spritecollide(self, game.walls, False):
-      self.rect = last
+      # Walls colissions
+      # RIGHT WALL COLLIDS FROM RIGHT
+      # KEEPS PLAYER OUTSIDE OF THE WINDOW
+      # WHICH IS BAD
+      cell = cell.rect
+      if last.right <= last.left and new.right > cell.left:
+        new.right = cell.left
+      if last.left >= cell.right and new.left < cell.right:
+        new.left = cell.right
+      if last.bottom <= cell.top and new.bottom > cell.top:
+        new.bottom = cell.top
+      if last.top >= cell.bottom and new.top < cell.bottom:
+        new.top = cell.bottom
 
 class Game(object):
   def main(self, screen):
